68acd3a28e86c80c5eed8483413ccb35aad33665
ohnanana <46968087+ohnanana@users.noreply.github.com>
2019-02-08 16:50:04 +0100

    Add files via upload

---
+++ corps.js
@@ -0,0 +1,30 @@
+$('.epaule').click(function()
+	{$('#epol').show();    
+	  
+});  
+
+$(".bras").click(function()
+{         
+	$("#brass").toggle(); 
+}); 
+
+$(".main").click(function()
+{  
+	  
+	$("#main1").toggle(); 
+});  
+
+$(".boutonHanche").click(function()
+{     
+	$('.affiche').hide();     
+	$("#textHanche").toggle(); });
+
+$(".boutonGenoux").click(function()
+{     
+	$('.affiche').hide();     
+	$("#textGenoux").toggle(); }); 
+
+$(".boutonPied").click(function()
+{     
+	$('.affiche').hide();     
+	$("#textPied").toggle(); }); 
